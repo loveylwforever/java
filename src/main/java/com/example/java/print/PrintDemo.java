4e0981224b2556b6eafd0694b8b57440811162a9
Jamie Gao <214142603@qq.com>
2022-09-04 15:01:13 +0800

    upgrade print class

--- src/main/java/com/example/java/print/PrintDemo.java
+++ src/main/java/com/example/java/print/PrintDemo.java
@@ -6,5 +6,11 @@ public class PrintDemo {
     public static void main(String[] args) {
         System.out.println("this is a simple print out");
         System.out.println("it can deal with escape character like \n or \t and something else");
+
+        System.out.print("this is a print that");
+        System.out.print(" don't change line\n");
+
+        System.out.printf("this is a print that can format %s", "word");
+
     }
 }
